e75733979069cb7e6c127fc2a9f6b12a765983e2
Aso Orji <42071241+asoorji@users.noreply.github.com>
2022-08-08 16:04:22 +0200

    Create guess_game.c

---
+++ guess_game.c
@@ -0,0 +1,40 @@
+/* Online C Compiler and Editor */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    int numberEntered, numberGenerated, contd;
+
+do{
+    printf("Guess a number (0 - 5): ");
+    scanf("%d", &numberEntered);
+    srand(time(0));
+    numberGenerated = (rand() % 5) + 1;
+    
+      if(numberEntered > numberGenerated){
+        printf("Too high!\n");
+        printf("The answer is %d\n", numberGenerated);
+        printf("Do you wish to continue?: (1 or 2): ");
+        scanf("%d", &contd);
+    }
+     else if(numberEntered < numberGenerated){
+        printf("Too low!\n");
+        printf("The answer is %d\n", numberGenerated);
+        printf("Do you wish to continue?: (1 or 2): ");
+        scanf("%d", &contd);
+    } 
+        else if (numberEntered == numberGenerated){
+        printf("Correct!\n");
+        printf("Your guess is %d", numberEntered);
+        printf(" and the number is also %d", numberGenerated);
+        printf("Do you wish to continue?: (1 or 2): ");
+        scanf("%d", &contd);
+    } 
+} while(contd == 1);
+ 
+printf("Thank You!");
+    
+return 0;
+}
